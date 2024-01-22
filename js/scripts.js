582b7478a1bbd3346577371b0f9ebc52a35b5a37
Argos Stone <peter@raedangroup.com>
2024-01-22 02:13:53 -0500

    Ex 1.6 Final Submission
    converted array items to buttons using DOM manipulation methods
    
    Included optional task of separating button listener to its own function
    
    buttons print item details to log

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 // Arrary of 3 Pokemon evolutions names, types, and heights
+// Currently grouped by region for possible future use
+// Could also be addressed instead by adding a region and starter classification property to each pokemon object
 
 let pokemonListKanto = [];
 pokemonListKanto[0] = {name: "Bulbasaur", height: 0.7, types: ["grass", " poison"]};
@@ -34,54 +36,94 @@ pokemonListHoenn[7] = {name: "Marshtomp", height: 0.7, types: ["water"]};
 pokemonListHoenn[8] = {name: "Swampert", height: 1.5, types: ["water"]};
 
 
+// not used in the lesson brief, but I may use it later for highlighting region starters for reference
     // IIEF containing foreach loop that prints region lists
-    let printPokemonList = (function () {
+    // let printPokemonList = (function () {
         
-        let printList = function (pokeList) {
-            pokeList.forEach(function (pokemon) {
-                    if (pokemon.height > 1.8) {
-                        document.write('<li class="bordered">' + pokemon.name + ' <b>(height: ' + pokemon.height + ')</b> - ' + pokemon.types + '</li>');
-                    } else {
-                        document.write('<li class="bordered">' + pokemon.name + ' (height: ' + pokemon.height + ') - ' + pokemon.types + '</li>');
-                    }
-                });
-        };
-        return {
-            regionList: printList
-        };
-    })();
+    //     let printList = function (pokeList) {
+    //         pokeList.forEach(function (pokemon) {
+    //                 if (pokemon.height > 1.8) {
+    //                     document.write('<li class="bordered">' + pokemon.name + ' <b>(height: ' + pokemon.height + ')</b> - ' + pokemon.types + '</li>');
+    //                 } else {
+    //                     document.write('<li class="bordered">' + pokemon.name + ' (height: ' + pokemon.height + ') - ' + pokemon.types + '</li>');
+    //                 }
+    //             });
+    //     };
+    //     return {
+    //         regionList: printList
+    //     };
+    // })();
 
-    let pokemonFavoritesRepository = (function () {
-        let favoritesList = []
+    let pokemonRepository = (function () {
 
+        // combined array of all 3 regions
+        let pokemonCombinedList = pokemonListKanto.concat(pokemonListJohto, pokemonListHoenn);
+
+        // add pokemon to combined array with validation
         let add = function (pokemon) {
             if (typeof pokemon.name === 'string' && typeof pokemon.height === 'number' && typeof pokemon.types === 'string') {
-                favoritesList.push(pokemon);
+                pokemonCombinedList.push(pokemon);
             } else {
-                console.error("Invalid Pokemon object");
+                console.error("Invalid Pokemon");
             }
         }
 
+        // return combined array
         let getAll = function () {
-            return favoritesList;
+            return pokemonCombinedList;
+        }
+
+        // add pokemon list item to DOM as buttons
+        function addListItem (pokemon) {
+            let pokemonList = document.querySelector('.pokemon-list');
+            let listPokemon = document.createElement('li');
+            let button = document.createElement('button');
+            button.innerText = pokemon.name;
+            button.classList.add('button-class');
+            listPokemon.appendChild(button);
+            pokemonList.appendChild(listPokemon);
+            addButtonClickListener(button, pokemon);
+        }
+
+        // button event listener to show details
+        function addButtonClickListener(button, pokemon) {
+            button.addEventListener('click', function (event) {
+                showDetails(pokemon);
+            });
+        }
+
+        // button content details passed to log
+        function showDetails (pokemon) {
+            console.log(pokemon);
         }
 
         return {
             add: add,
-            getAll: getAll
+            getAll: getAll,
+            addListItem: addListItem
         }
     })()
     
-    //   Add my favorite pokemon to the Favorites List
-    pokemonFavoritesRepository.add({ name: 'Arcanine', height: 1.9, types: 'fire'});
+    //   Add my Growlithe and Arcenine pokemon to the grouped Pokemon List
+    pokemonRepository.add({ name: 'Growlithe', height: 0.7, types: 'fire'});
+    pokemonRepository.add({ name: 'Arcanine', height: 1.9, types: 'fire'});
 
+    // Write Pokemon List to the DOM
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+
+// not used in the lesson brief, but I may use it later for highlighting region starters for reference
     // Write Region Lists to the DOM
-    document.write('<h2>Kanto Starters</h2>');
-    printPokemonList.regionList(pokemonListKanto);
-    document.write('</br><h2>Johto Starters</h2>');
-    printPokemonList.regionList(pokemonListJohto);
-    document.write('</br><h2>Hoenn Starters</h2>');
-    printPokemonList.regionList(pokemonListHoenn);
+    // document.write('<h2>Kanto Starters</h2>');
+    // printPokemonList.regionList(pokemonListKanto);
+    // document.write('</br><h2>Johto Starters</h2>');
+    // printPokemonList.regionList(pokemonListJohto);
+    // document.write('</br><h2>Hoenn Starters</h2>');
+    // printPokemonList.regionList(pokemonListHoenn);
 
-    // Log Favorites List array to console
-    console.log(pokemonFavoritesRepository.getAll());
+    // document.write('</br><h2>All Pokemon</h2>');
+    // printPokemonList.regionList(pokemonRepository.getAll());
+
+    // Log grouped Pokemon List array to console
+    // console.log(pokemonRepository.getAll());
